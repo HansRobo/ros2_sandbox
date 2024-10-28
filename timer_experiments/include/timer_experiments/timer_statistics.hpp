6b262a42ce9eec79e794f7900334439cb803fa36
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-10-29 08:36:42 +0900

    normal_timer.cpp

---
+++ timer_experiments/include/timer_experiments/timer_statistics.hpp
@@ -0,0 +1,43 @@
+//
+// Created by hans on 24/10/27.
+//
+
+#ifndef REALTIME_BUFFER_HPP
+#define REALTIME_BUFFER_HPP
+
+#include <vector>
+#include <iostream>
+#include <chrono>
+#include <../magic_enum.hpp>
+
+
+// enum class DefaultSeries{
+//   DEFAULT,
+// };
+//
+// template <typename Series = DefaultSeries
+class TimerStatistics {
+  public:
+    using clock_type = std::chrono::high_resolution_clock;
+    explicit TimerStatistics(int buffer_reserve_size = 1000) {
+      records.reserve(buffer_reserve_size);
+    }
+
+    void record_now() {
+      records.push_back(clock_type::now());
+    }
+
+    [[nodiscard]] auto to_durations() const
+    {
+      std::vector<std::chrono::duration<double>> durations;
+      durations.reserve(records.size());
+      for (size_t i = 1; i < records.size(); i++) {
+        durations.emplace_back(records[i] - records[i - 1]);
+      }
+      return durations;
+    }
+
+    // std::vector<std::chrono::time_point> buffer[magic_enum::enum_count<Series>()];
+    std::vector<clock_type::time_point> records;
+};
+#endif //REALTIME_BUFFER_HPP
