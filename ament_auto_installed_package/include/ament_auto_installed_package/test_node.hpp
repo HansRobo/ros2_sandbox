5106920cd93ea558795775c6dfee56ff66ca6701
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-04-06 10:24:03 +0900

    split a aprt of source file to header file

---
+++ ament_auto_installed_package/include/ament_auto_installed_package/test_node.hpp
@@ -0,0 +1,14 @@
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <example_interfaces/msg/string.hpp>
+
+class TestNode : public rclcpp::Node
+{
+public:
+  explicit TestNode(rclcpp::NodeOptions options)
+  : Node("ament_auto_installed_package_node", options), publisher(create_publisher<example_interfaces::msg::String>("topic", 10))
+  {}
+
+private:
+  rclcpp::Publisher<example_interfaces::msg::String>::SharedPtr publisher;
+};
--- ament_auto_installed_package/src/ament_auto_installed_package_node.cpp
+++ ament_auto_installed_package/src/ament_auto_installed_package_node.cpp
@@ -1,20 +1,7 @@
 
-#include <chrono>
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
-#include <example_interfaces/msg/string.hpp>
-#include <unordered_map>
-
-class TestNode : public rclcpp::Node
-{
-public:
-  explicit TestNode(rclcpp::NodeOptions options)
-  : Node("ament_auto_installed_package_node", options), publisher(create_publisher<example_interfaces::msg::String>("topic", 10))
-  {}
-
-private:
-  rclcpp::Publisher<example_interfaces::msg::String>::SharedPtr publisher;
-};
+#include <ament_auto_installed_package/test_node.hpp>
 
 int main(int argc, char ** argv)
 {
